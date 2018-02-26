dae5e19e565fd0f61bde6b993c5813b895042f73
Szymon Żogała <szymek.655@gmail.com>
2018-02-26 23:27:44 +0100

    Added Menu

---
+++ UWPEnhanced/Controls/Menu/Enums/Enums.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPEnhanced.Controls
+{
+	/// <summary>
+	/// Position for the menu control
+	/// </summary>
+	public enum MenuPosition
+	{
+		/// <summary>
+		/// Menu is on the left, opens to the right, content is stored vertically
+		/// </summary>
+		Left = 0,
+
+		/// <summary>
+		/// Menu is on the top, opens to the bottom, content is stored horizontally
+		/// </summary>
+		Top = 1,
+
+		/// <summary>
+		/// Menu is on the right, opens to the left, content is stored vertically
+		/// </summary>
+		Right = 2,
+
+		/// <summary>
+		/// Menu is on the bottom, opens to the top, content is stored horizontally
+		/// </summary>
+		Bottom = 3,
+	}
+}
---
+++ UWPEnhanced/Controls/Menu/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace UWPEnhanced.Controls
+{
+    public sealed class Menu : Control
+    {
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public Menu()
+        {
+            this.DefaultStyleKey = typeof(Menu);
+			t();
+        }
+
+		private async Task t()
+		{
+			await Task.Delay(2000);
+			VisualStateManager.GoToState(this, "Test1", false);
+		}
+
+		#endregion
+
+		#region MenuPositionProperty Dependency Property
+
+		/// <summary>
+		/// Position of the menu
+		/// </summary>
+		public MenuPosition MenuPositionProperty
+		{
+			get => (MenuPosition)GetValue(MenuPositionPropertyProperty);
+			set => SetValue(MenuPositionPropertyProperty, value);
+		}
+
+		/// <summary>
+		/// Position of the menu
+		/// </summary>
+		public static readonly DependencyProperty MenuPositionPropertyProperty =
+			DependencyProperty.Register(nameof(MenuPositionProperty), typeof(MenuPosition),
+			typeof(Menu), new PropertyMetadata(MenuPosition.Left));
+
+		#endregion
+
+
+	}
+}
