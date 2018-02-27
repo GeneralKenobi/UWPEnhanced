1ac6522fc529811332e31bd7bed37bbea70cdf64
Szymon Żogała <szymek.655@gmail.com>
2018-02-27 22:58:47 +0100

    Work in progress on moving between states using VisualStateNavigator

---
+++ UWPEnhanced/Xaml/VisualStateNavigator/VisualStateNavigation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace UWPEnhanced.Xaml
+{
+	public class VisualStateNavigation : DependencyObject
+	{
+		#region TransferToState Dependency Property
+
+		/// <summary>
+		/// State to transfer to
+		/// </summary>
+		public string TransferToState
+		{
+			get => (string)GetValue(TransferToStateProperty);
+			set => SetValue(TransferToStateProperty, value);
+		}
+
+		/// <summary>
+		/// Backing store for <see cref="TransferToState"/>
+		/// </summary>
+		public static readonly DependencyProperty TransferToStateProperty =
+			DependencyProperty.Register(nameof(TransferToState), typeof(string),
+			typeof(VisualStateNavigation), new PropertyMetadata(default(string)));
+
+		#endregion	
+	}
+}
---
+++ UWPEnhanced/Xaml/VisualStateNavigator/VisualStateNavigator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace UWPEnhanced.Xaml
+{
+	public class VisualStateNavigator : DependencyObject
+	{
+		
+		#region Navigations
+
+		/// <summary>
+		/// Getter for <see cref="Navigations"/>
+		/// </summary>
+		public static ObservableCollection<VisualStateNavigation> GetNavigations(DependencyObject obj) => (ObservableCollection<VisualStateNavigation>)obj.GetValue(NavigationsProperty);
+
+		/// <summary>
+		/// Setter for <see cref="Navigations"/>
+		/// </summary>
+		public static void SetNavigations(DependencyObject obj, ObservableCollection<VisualStateNavigation> value) => obj.SetValue(NavigationsProperty, value);
+
+		/// <summary>
+		/// Collection of all navigations defined for the given object
+		/// </summary>
+		public static readonly DependencyProperty NavigationsProperty =
+			DependencyProperty.RegisterAttached("Navigations", typeof(ObservableCollection<VisualStateNavigation>),
+			 typeof(VisualStateNavigator), new PropertyMetadata(default(ObservableCollection<VisualStateNavigation>)));
+
+		#endregion
+	}
+}
