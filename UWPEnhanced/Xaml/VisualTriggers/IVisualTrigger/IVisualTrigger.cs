5001c8f356b0dd457841601183eba9df77c5f9b7
Szymon Żogała <szymek.655@gmail.com>
2018-03-11 13:18:54 +0100

    Added interface for visual triggers

---
+++ UWPEnhanced/Xaml/VisualTriggers/IVisualTrigger/IVisualTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// Interface for triggers used in xaml
+	/// </summary>
+	public interface IVisualTrigger
+	{
+		/// <summary>
+		/// Event fired when the trigger is triggered
+		/// </summary>
+		EventHandler Triggered { get; }
+	}
+}
