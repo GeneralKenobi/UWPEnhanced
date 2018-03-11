632a26ee3079398313e8a47759d44f3c27b5129d
Szymon Żogała <szymek.655@gmail.com>
2018-03-11 13:20:18 +0100

    Standard implementation of IVisualAttachment

--- UWPEnhanced/Xaml/VisualAttachments/VisualAttachment/VisualAttachment.cs
+++ UWPEnhanced/Xaml/VisualAttachments/VisualAttachment/VisualAttachment.cs
@@ -54,7 +54,7 @@ namespace UWPEnhanced.Xaml
 		/// Attaches this <see cref="IAttachable"/> to the specified <see cref="DependencyObject"/>
 		/// </summary>
 		/// <param name="obj"></param>
-		public void Attach(DependencyObject obj)
+		public virtual void Attach(DependencyObject obj)
 		{
 			// Skip if the obj is the same as the stored object
 			if(AttachedTo == obj)
@@ -70,7 +70,7 @@ namespace UWPEnhanced.Xaml
 			}
 
 			// Debug information for null obj
-			Debug.Assert(obj == null, "Can't attach self to a null object");
+			Debug.Assert(obj != null, "Can't attach self to a null object");
 
 			// Assign the obj to AttachedTo; If obj == null, throw ArgumentException instead
 			AttachedTo = obj ?? throw new ArgumentNullException(nameof(obj));
@@ -80,7 +80,7 @@ namespace UWPEnhanced.Xaml
 		/// Detaches this <see cref="IAttachable"/> from the <see cref="DependencyObject"/> it is attached to
 		/// (if it's attached to begin with)
 		/// </summary>
-		public void Detach() => AttachedTo = null;		
+		public virtual void Detach() => AttachedTo = null;
 
 		#endregion
 	}
