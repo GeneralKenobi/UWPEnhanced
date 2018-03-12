115da128b6d2a158231bf80db8f0811b7c2c1d98
Szymon Żogała <szymek.655@gmail.com>
2018-03-12 11:29:19 +0100

    Added TryFindParent version, uses out parameter and boolean return type to indicate success

--- UWPEnhanced/Xaml/VisualTreeHelpers/VisualTreeHelpers.cs
+++ UWPEnhanced/Xaml/VisualTreeHelpers/VisualTreeHelpers.cs
@@ -30,5 +30,21 @@ namespace UWPEnhanced.Xaml
 			
 			return obj as T;
 		}
+
+		/// <summary>
+		/// Tries to find a parent of type <typeparamref name="T"/> and assign it to <paramref name="parent"/>. If the parent is
+		/// found, returns true, if it is not found assigns null and returns false
+		/// </summary>
+		/// <typeparam name="T">Type of the parent to look for</typeparam>
+		/// <param name="obj"><see cref="DependencyObject"/> whose parent to look for</param>
+		/// <param name="parent">Variable to assign the parent to</param>
+		/// <returns></returns>
+		public static bool TryFindParent<T>(DependencyObject obj, out T parent)
+			where T : class
+		{
+			parent = FindParent<T>(obj);
+
+			return parent != null;
+		}
 	}
 }
