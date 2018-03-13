45ca82fc3f23838721beb4187c401de8f9e8e34b
Szymon Żogała <szymek.655@gmail.com>
2018-03-13 22:07:06 +0100

    Added PointerTriggerCommand

---
+++ UWPEnhanced/Xaml/VisualTriggers/VisualPointerTrigger/PointerTriggerCommand/PointerTriggerCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// Command which is triggered by a single <see cref="VisualPointerTrigger"/>
+	/// </summary>
+	public class PointerTriggerCommand : VisualPointerTrigger
+	{
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public PointerTriggerCommand()
+		{
+			this.Triggered += (s,e) => Command.Execute(null);
+		}
+
+		#region Command Dependency Property
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public ICommand Command
+		{
+			get => (ICommand)GetValue(CommandProperty);
+			set => SetValue(CommandProperty, value);
+		}
+
+		/// <summary>
+		/// Backing store for <see cref="Command"/>
+		/// </summary>
+		public static readonly DependencyProperty CommandProperty =
+			DependencyProperty.Register(nameof(Command), typeof(ICommand),
+			typeof(PointerTriggerCommand), new PropertyMetadata(default(ICommand)));
+
+		#endregion
+	}
+}
