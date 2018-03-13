9a1cea5a2eb16c4f34b86b2e1d8ab3835deb2854
Szymon Żogała <szymek.655@gmail.com>
2018-03-14 00:12:09 +0100

    Work in progress on VisualTriggers

--- TestEnvironment/MainPage.xaml.cs
+++ TestEnvironment/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using UWPEnhanced.Xaml;
 using Windows.System;
 using Windows.UI.Xaml;
@@ -12,19 +16,27 @@ namespace TestEnvironment
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	public sealed partial class MainPage : Page
+	public sealed partial class MainPage : Page, INotifyPropertyChanged
     {
         public MainPage()
         {
+			test = new RelayCommand(() => Debugger.Break());
 			this.InitializeComponent();
 			task();
         }
 
+		public int d { get; set; } = 0;
 
 		private async Task task()
 		{
 			await Task.Delay(1000);
+			d = 5;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("d"));
 			(this.Resources["sb"] as Storyboard)?.Begin();
 		}
+
+		public ICommand test;
+
+		public event PropertyChangedEventHandler PropertyChanged;
 	}
 }
