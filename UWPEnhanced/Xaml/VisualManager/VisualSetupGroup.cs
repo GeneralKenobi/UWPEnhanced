7d99db1e377fc04fc44d93ae5162ba4c09bde6fb
Szymon Żogała <szymek.655@gmail.com>
2018-03-27 13:41:34 +0200

    Adds/removes visual state group from the element, handles addition and deletion of old states

--- UWPEnhanced/Xaml/VisualManager/VisualSetupGroup.cs
+++ UWPEnhanced/Xaml/VisualManager/VisualSetupGroup.cs
@@ -12,41 +12,78 @@ namespace UWPEnhanced.Xaml
 	/// </summary>
 	public class VisualSetupGroup : AttachableDependencyCollectionOfT<VisualSetup>
 	{
-		
-
-
-		protected override VisualSetup NewElementCheckRoutine(DependencyObject item)
-		{
-			var baseChecked =  base.NewElementCheckRoutine(item);
-
-			if(VisualTreeHelpers.TryFindParentOrSelf(baseChecked, out FrameworkElement parent))
-			{
-				var registeredGroups = VisualStateManager.GetVisualStateGroups(parent);
-
-				if(registeredGroups.Count > 0)
-				{
-					var primaryGroup = registeredGroups[0];
-					
-				}
-			}
-
-			return baseChecked;
-		}
-
-		#region Public Methods
+		/// <summary>
+		/// The group used by this instance to store its visual state setups
+		/// </summary>
+		private VisualStateGroup _AssociatedVisualStateGroup = null;
 
 		/// <summary>
-		/// Adds a <see cref="VisualSetup"/> to the container
+		/// Before attaching checks if the <paramref name="obj"/> is a <see cref="FrameworkElement"/>, then adds the visual
+		/// group to the element's visual groups.
 		/// </summary>
-		/// <param name="setup"></param>
-		public void AddVisualSetup(VisualSetup setup)
+		/// <param name="obj"></param>
+		public override void Attach(DependencyObject obj)
 		{
-			//if(!_AssociatedVisualSetups.Contains(setup))
-			//{
-			//	_AssociatedVisualSetups.Add(setup);
-			//}
+			if (obj is FrameworkElement element)
+			{
+				base.Attach(obj);
+
+				if (_AssociatedVisualStateGroup == null)
+				{
+					// Create a new group dedicated for the VisualStateManager
+					_AssociatedVisualStateGroup = new VisualStateGroup();
+				}
+				// Add the items
+				foreach(var item in this)
+				{
+					if(item is VisualSetup setup)
+					{
+						_AssociatedVisualStateGroup.States.Add(setup.State);
+					}
+				}
+
+				// Add the group to the element
+				VisualStateManager.GetVisualStateGroups(element).Insert(0, _AssociatedVisualStateGroup);
+			}
+			else
+			{
+				throw new ArgumentException("Visual states can be added only to " + nameof(FrameworkElement) + "s");
+			}
 		}
 
-		#endregion
+		/// <summary>
+		/// Detatches the group and removes the group from the <see cref="_AssociatedVisualStateGroup"/>
+		/// </summary>
+		public override void Detach()
+		{
+			VisualStateManager.GetVisualStateGroups(AttachedTo as FrameworkElement).Remove(_AssociatedVisualStateGroup);
+
+			base.Detach();
+		}
+
+		/// <summary>
+		/// Checks the new item and adds it to the visual group
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
+		protected override VisualSetup NewElementCheckRoutine(DependencyObject item)
+		{
+			var checkedItem = base.NewElementCheckRoutine(item);
+
+			_AssociatedVisualStateGroup.States.Add(checkedItem.State);
+
+			return checkedItem;
+		}
+		
+		/// <summary>
+		/// Removes the item from the <see cref="_AssociatedVisualStateGroup"/> and detatches it
+		/// </summary>
+		/// <param name="item"></param>
+		protected override void CleanupRoutine(VisualSetup item)
+		{
+			base.CleanupRoutine(item);
+
+			_AssociatedVisualStateGroup.States.Remove(item.State);
+		}
 	}
 }
