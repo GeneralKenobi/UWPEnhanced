aefe453ebe528497a4dfb6acf4fe8503c9ee8a34
Szymon Żogała <szymek.655@gmail.com>
2018-04-03 15:25:26 +0200

    Added

---
+++ UWPEnhanced/Helpers/ThreadingHelpers/DispatcherHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+
+namespace UWPEnhanced.Helpers
+{
+	/// <summary>
+	/// Class containing helper methods related to
+	/// <see cref="Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher"/>
+	/// </summary>
+	public static class DispatcherHelpers
+	{
+		/// <summary>
+		/// Runs the <see cref="DispatchedHandler"/> and waits (blocks execution) until it completes.
+		/// </summary>
+		/// <param name="dispatchedHandler">Action to perform</param>
+		/// <param name="priority">Priority to assign to it</param>
+		public static void Run(DispatchedHandler dispatchedHandler,
+			CoreDispatcherPriority priority = CoreDispatcherPriority.Normal) =>
+			RunAsync(dispatchedHandler, priority).Wait();
+			
+		/// <summary>
+		/// Runs the <see cref="DispatchedHandler"/> and returns a task that will complete when the action has completed.
+		/// </summary>
+		/// <param name="dispatchedHandler">Action to perform</param>
+		/// <param name="priority">Priority to assign to it</param>
+		public static Task RunAsync(DispatchedHandler dispatchedHandler,
+			CoreDispatcherPriority priority = CoreDispatcherPriority.Normal) =>		
+			Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+				priority, dispatchedHandler).AsTask();
+	}
+}
