d5c84d3fa7b45d001cf3771454d576cd11f3aa84
Szymon Żogała <szymek.655@gmail.com>
2018-04-12 22:41:14 +0200

    Added

---
+++ UWPEnhanced/Xaml/VisualTriggers/TriggerCollection/VisualTransition.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// Allows to condense transitions to a single state triggered by different <see cref="IVisualTrigger"/>
+	/// into a single object
+	/// </summary>
+	public class VisualTransition : VisualAttachmentCollection<IVisualTrigger>
+	{
+		#region Events
+
+		/// <summary>
+		/// Event fired whenever one of the contained triggers fires
+		/// </summary>
+		public EventHandler<VisualTransitionTriggeredEventArgs> TransitionTriggered;
+
+		#endregion
+
+		#region TransitionTo Dependency Property
+
+		/// <summary>
+		/// Name of the instance to transition to
+		/// </summary>
+		public string TransitionTo
+		{
+			get => (string)GetValue(TransitionToProperty);
+			set => SetValue(TransitionToProperty, value);
+		}
+
+		/// <summary>
+		/// Backing store for <see cref="TransitionTo"/>
+		/// </summary>
+		public static readonly DependencyProperty TransitionToProperty =
+			DependencyProperty.Register(nameof(TransitionTo), typeof(string),
+			typeof(VisualTransition), new PropertyMetadata(default(string)));
+
+		#endregion
+
+		#region Protected Methods
+
+		/// <summary>
+		/// After calling base version subscribes to <see cref="IVisualTrigger.Triggered"/> event
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
+		protected override IVisualTrigger NewElementCheckRoutine(DependencyObject item)
+		{
+			var baseCheck = base.NewElementCheckRoutine(item);
+
+			baseCheck.Triggered += TriggerFired;
+
+			return baseCheck;
+		}
+
+		/// <summary>
+		/// Before calling base version unsubscribes from <see cref="IVisualTrigger.Triggered"/> event
+		/// </summary>
+		/// <param name="item"></param>
+		/// <returns></returns>
+		protected override void CleanupRoutine(IVisualTrigger item)
+		{
+			item.Triggered -= TriggerFired;
+
+			base.CleanupRoutine(item);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Used to subscribe to contained events; Invokes <see cref="TransitionTriggered"/> event
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="args"></param>
+		private void TriggerFired(object sender, EventArgs args) =>
+			TransitionTriggered?.Invoke(this, new VisualTransitionTriggeredEventArgs(
+				sender as IVisualTrigger, TransitionTo));
+
+		#endregion
+	}
+}
---
+++ UWPEnhanced/Xaml/VisualTriggers/TriggerCollection/VisualTransitionTriggeredEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// Arguments for VisualTransitionEvent
+	/// </summary>
+	public class VisualTransitionTriggeredEventArgs : EventArgs
+	{
+		/// <summary>
+		/// <see cref="IVisualTrigger"/> that was resposnible for the trigger
+		/// </summary>
+		public IVisualTrigger TriggeredEvent { get; private set; }
+
+		/// <summary>
+		/// Name of the instance to transition to
+		/// </summary>
+		public string TransitionTo { get; private set; }
+
+		/// <summary>
+		/// Default constructor, requires parameters
+		/// </summary>
+		/// <exception cref="ArgumentNullException">For <paramref name="trigger"/></exception>
+		public VisualTransitionTriggeredEventArgs(IVisualTrigger trigger, string transitionTo)
+		{
+			TriggeredEvent = trigger ?? throw new ArgumentNullException(nameof(trigger));
+			TransitionTo = transitionTo;
+		}
+	}
+}
