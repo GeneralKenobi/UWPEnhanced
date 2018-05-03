7339a36396ee20d96e50cfbde10a35fe763d0d7c
Szymon Żogała <szymek.655@gmail.com>
2018-05-03 21:35:27 +0200

    Added

---
+++ UWPEnhanced/Controls/Menu/VisualDataTriggers/VisualDataTriggerMenuPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWPEnhanced.Controls;
+using Windows.UI.Xaml;
+
+namespace UWPEnhanced.Xaml
+{
+	public class VisualDataTriggerMenuPosition : GenericVisualDataTrigger<MenuPosition>
+	{
+		
+	}
+}
