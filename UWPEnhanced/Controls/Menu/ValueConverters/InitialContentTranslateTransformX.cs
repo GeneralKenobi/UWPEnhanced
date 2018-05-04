90c462fe4c09e81c3dabaf839f6ed2aec860ff9b
Szymon Żogała <szymek.655@gmail.com>
2018-05-04 19:48:15 +0200

    Added

---
+++ UWPEnhanced/Controls/Menu/ValueConverters/InitialContentTranslateTransformX.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace UWPEnhanced.Controls
+{
+	/// <summary>
+	/// Converter for the intial value of <see cref="TranslateTransform.X"/> of ContentPresenter in the
+	/// <see cref="Menu"/>. Value should be the <see cref="Menu.Position"/>. In all other cases 0 is returned.
+	/// </summary>
+	public class InitialContentTranslateTransformX : IValueConverter
+    {
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if(value is Menu menu)
+			{
+				switch (menu.Position)
+				{
+					case MenuPosition.Left:
+						{
+							return -menu.ContentLength;
+						}
+
+					case MenuPosition.Right:
+						{
+							return menu.ContentLength;
+						}
+				}
+			}
+
+			return 0;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
