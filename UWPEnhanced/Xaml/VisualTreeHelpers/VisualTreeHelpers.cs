309cb6b850c068efd3d1440b9134d6203cd8d7a8
Szymon Żogała <szymek.655@gmail.com>
2018-05-06 17:22:10 +0200

    Added methods looking for a child, changed the already existing into extension methods

--- UWPEnhanced/Xaml/VisualTreeHelpers/VisualTreeHelpers.cs
+++ UWPEnhanced/Xaml/VisualTreeHelpers/VisualTreeHelpers.cs
@@ -13,7 +13,7 @@ namespace UWPEnhanced.Xaml
 	/// </summary>
 	public static class VisualTreeHelpers
 	{
-		#region Find Parent of type T
+		#region Find parent of type T
 
 		/// <summary>
 		/// Finds and returns a parent of type <typeparamref name="T"/> or null if the parent wasn't found.
@@ -22,15 +22,15 @@ namespace UWPEnhanced.Xaml
 		/// <typeparam name="T">Type of the parent to look for</typeparam>
 		/// <param name="obj"><see cref="DependencyObject"/> whose parent to look for</param>
 		/// <returns></returns>
-		public static T FindParentOrSelf<T>(DependencyObject obj)
+		public static T FindParentOrSelf<T>(this DependencyObject obj)
 			where T : class
 		{
 			// Keep looking until either the object becomes null or parent of type T is found
-			while(obj != null && !(obj is T))
+			while (obj != null && !(obj is T))
 			{
 				obj = VisualTreeHelper.GetParent(obj);
-			}			
-			
+			}
+
 			return obj as T;
 		}
 
@@ -43,7 +43,7 @@ namespace UWPEnhanced.Xaml
 		/// <param name="obj"><see cref="DependencyObject"/> whose parent to look for</param>
 		/// <param name="parent">Variable to assign the parent to</param>
 		/// <returns></returns>
-		public static bool TryFindParentOrSelf<T>(DependencyObject obj, out T parent)
+		public static bool TryFindParentOrSelf<T>(this DependencyObject obj, out T parent)
 			where T : class
 		{
 			parent = FindParentOrSelf<T>(obj);
@@ -57,7 +57,7 @@ namespace UWPEnhanced.Xaml
 		/// <typeparam name="T">Type of the parent to look for</typeparam>
 		/// <param name="obj"><see cref="DependencyObject"/> whose parent to look for</param>
 		/// <returns></returns>
-		public static T FindParent<T>(DependencyObject obj)
+		public static T FindParent<T>(this DependencyObject obj)
 			where T : class => obj == null ? null : FindParentOrSelf<T>(VisualTreeHelper.GetParent(obj));
 
 		/// <summary>
@@ -68,10 +68,10 @@ namespace UWPEnhanced.Xaml
 		/// <param name="obj"><see cref="DependencyObject"/> whose parent to look for</param>
 		/// <param name="parent">Variable to assign the parent to</param>
 		/// <returns></returns>
-		public static bool TryFindParent<T>(DependencyObject obj, out T parent)
+		public static bool TryFindParent<T>(this DependencyObject obj, out T parent)
 			where T : class
 		{
-			if(obj == null)
+			if (obj == null)
 			{
 				parent = null;
 				return false;
@@ -80,6 +80,156 @@ namespace UWPEnhanced.Xaml
 			{
 				return TryFindParentOrSelf(VisualTreeHelper.GetParent(obj), out parent);
 			}
+		}
+
+		#endregion
+
+		#region Find child
+
+		/// <summary>
+		/// Finds and returns the first occurance of a child of type <typeparamref name="T"/>. Search order is branch by branch
+		/// </summary>
+		/// <typeparam name="T">Type to look for</typeparam>
+		/// <param name="obj">Object whose children to search through</param>
+		/// <returns>First found child of type T or null if none was found</returns>
+		private static T FindChild<T>(this DependencyObject obj)
+			where T : DependencyObject
+		{
+			// For each child
+			for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); ++i)
+			{
+				// Get it
+				var nextChild = VisualTreeHelper.GetChild(obj, i);
+				
+				// If it's of type T, return it
+				if (nextChild is T casted)
+				{
+					return casted;
+				}
+
+				// Otherwise search through its children
+				var subSearchResult = nextChild.FindChild<T>();
+
+				// And if there was a child of type T
+				if(subSearchResult != null)
+				{
+					// Return it
+					return subSearchResult;
+				}
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Finds the first occurance of a child of type <typeparamref name="T"/>. Search order is branch by branch.
+		/// If the child was found it is stored in <paramref name="child"/> and true is returned.
+		/// </summary>
+		/// <typeparam name="T">Type to look for</typeparam>
+		/// <param name="obj">Object whose children to search through</param>
+		/// <param name="child">Reference to store the found child in</param>
+		/// <returns></returns>
+		public static bool TryFindChild<T>(this DependencyObject obj, out T child)
+			where T : DependencyObject
+		{
+			child = FindChild<T>(obj);
+			return child != null;
+		}
+
+		/// <summary>
+		/// Returns all children that are of type T
+		/// </summary>
+		/// <typeparam name="T">Type to look for</typeparam>
+		/// <param name="obj">Object whose children to search through</param>
+		/// <returns></returns>
+		public static IEnumerable<T> FindChildren<T>(this DependencyObject obj)
+			where T : DependencyObject
+		{
+			// For each child of the object
+			for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); ++i)
+			{
+				// Get the next child
+				var nextChild = VisualTreeHelper.GetChild(obj, i);
+
+				// If it'ss of type T, return it aswell
+				if (nextChild is T casted)
+				{
+					yield return casted;
+				}
+
+				// Search through its children and return all found occurances
+				foreach(var item in FindChildren<T>(nextChild))
+				{
+					yield return item;
+				}
+			}
+		}
+
+		#endregion
+
+		#region Find child or self
+
+		/// <summary>
+		/// Finds and returns the first occurance of a child (or self) of type <typeparamref name="T"/>.
+		/// Search order is branch by branch
+		/// </summary>
+		/// <typeparam name="T">Type to look for</typeparam>
+		/// <param name="obj">Object whose children to search through</param>
+		/// <returns>First found child of type T or null if none was found</returns>
+		private static T FindChildOrSelf<T>(this DependencyObject obj)
+			where T : DependencyObject
+		{
+			if(obj is T castedObj)
+			{
+				return castedObj;
+			}
+			else
+			{
+				return FindChild<T>(obj);
+			}			
+		}
+
+		/// <summary>
+		/// Finds the first occurance of a child (or self) of type <typeparamref name="T"/>. Search order is branch by branch.
+		/// If the child was found it is stored in <paramref name="child"/> and true is returned.
+		/// </summary>
+		/// <typeparam name="T">Type to look for</typeparam>
+		/// <param name="obj">Object whose children to search through</param>
+		/// <param name="child">Reference to store the found child in</param>
+		/// <returns></returns>
+		public static bool TryFindChildOrSelf<T>(this DependencyObject obj, out T child)
+			where T : DependencyObject
+		{
+			if(obj is T casted)
+			{
+				child = casted;
+			}
+			else
+			{
+				child = FindChild<T>(obj);
+			}
+
+			return child != null;
+		}
+
+		/// <summary>
+		/// Returns all children (self as well) that are of type T
+		/// </summary>
+		/// <typeparam name="T">Type to look for</typeparam>
+		/// <param name="obj">Object whose children to search through</param>
+		/// <returns></returns>
+		public static IEnumerable<T> FindChildrenOrSelf<T>(this DependencyObject obj)
+			where T : DependencyObject
+		{
+			if (obj is T casted)
+			{
+				yield return casted;
+			}
+
+			foreach (var item in FindChildren<T>(obj))
+			{
+				yield return item;
+			}			
 		}
 
 		#endregion
