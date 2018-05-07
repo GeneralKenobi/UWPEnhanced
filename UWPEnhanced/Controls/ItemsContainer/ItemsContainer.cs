7bab3ba57d6179897b62cb289dce34b714235298
Szymon Żogała <szymek.655@gmail.com>
2018-05-07 13:07:52 +0200

    Item positioning is done

--- UWPEnhanced/Controls/ItemsContainer/ItemsContainer.cs
+++ UWPEnhanced/Controls/ItemsContainer/ItemsContainer.cs
@@ -18,18 +18,157 @@ namespace UWPEnhanced.Controls
 {
     public sealed class ItemsContainer : ItemsControl
     {
-        public ItemsContainer()
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public ItemsContainer()
         {
-            this.DefaultStyleKey = typeof(ItemsContainer);			
+            this.DefaultStyleKey = typeof(ItemsContainer);
+			this.Loaded += ItemsContainerLoaded;
         }
 
-		protected override Size ArrangeOverride(Size finalSize)
+		/// <summary>
+		/// When the control is loaded assigns the first <see cref="ItemSpacing"/> value to the <see cref="UnderlyingPanel"/>
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ItemsContainerLoaded(object sender, RoutedEventArgs e)
 		{
-			var a = VisualTreeHelpers.FindChild<ItemsContainerPanel>(this);
-			a.ItemSpacing = 30;
-			a.Arrange(new Rect(new Point(0, 0), finalSize));
-
-			return finalSize;
+			UpdateDirectionOnUnderylingPanel();
+			UpdateItemSpacingOnUnderylingPanel();
 		}
+		
+		#endregion
+
+		#region Private members
+
+		/// <summary>
+		/// Backing store for <see cref="UnderlyingPanel"/>
+		/// </summary>
+		private ItemsContainerPanel _UnderlyingPanel = null;
+
+		#endregion
+
+		#region Private properties
+
+		/// <summary>
+		/// The <see cref="ItemsContainerPanel"/> that is associated with this control
+		/// </summary>
+		private ItemsContainerPanel UnderlyingPanel => (_UnderlyingPanel != null || this.TryFindChild(out _UnderlyingPanel)) ?
+			_UnderlyingPanel : null;
+
+		#endregion
+
+		#region Public Properties
+
+		/// <summary>
+		/// The default spacing between items
+		/// </summary>
+		public static double DefaultItemSpacing => 5;
+
+		/// <summary>
+		/// The default direction of items
+		/// </summary>
+		public static ItemsDirection DefaultFlowDirection => ItemsDirection.TopToBottom;
+
+		#endregion
+
+		#region ItemSpacing Dependency Property
+
+		/// <summary>
+		/// Space between the items. Default is 5
+		/// </summary>
+		public double ItemSpacing
+		{
+			get => (double)GetValue(ItemSpacingProperty);
+			set => SetValue(ItemSpacingProperty, value);
+		}
+
+		/// <summary>
+		/// Backing store for <see cref="ItemSpacing"/>
+		/// </summary>
+		public static readonly DependencyProperty ItemSpacingProperty =
+			DependencyProperty.Register(nameof(ItemSpacing), typeof(double),
+			typeof(ItemsContainer), new PropertyMetadata(DefaultItemSpacing, ItemSpacingChanged));
+
+		#endregion
+
+		#region Direction Dependency Property
+
+		/// <summary>
+		/// Direction of item placement in this container. Hides the inherited <see cref="FrameworkElement.FlowDirection"/>.
+		/// </summary>
+		public new ItemsDirection FlowDirection
+		{
+			get => (ItemsDirection)GetValue(FlowDirectionProperty);
+			set => SetValue(FlowDirectionProperty, value);
+		}
+
+		/// <summary>
+		/// Backing store for <see cref="FlowDirection"/>
+		/// </summary>
+		public static readonly DependencyProperty FlowDirectionProperty =
+			DependencyProperty.Register(nameof(FlowDirection), typeof(ItemsDirection),
+			typeof(ItemsContainer), new PropertyMetadata(DefaultFlowDirection, DirectionChanged));
+
+		#endregion
+
+		#region Private static methods
+
+		/// <summary>
+		/// Callback for when <see cref="ItemSpacingProperty"/> changes
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="e"></param>
+		private static void ItemSpacingChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
+		{
+			if (s is ItemsContainer container && e.NewValue != e.OldValue)
+			{
+				container.UpdateItemSpacingOnUnderylingPanel();
+			}
+		}
+
+		/// <summary>
+		/// Callback for when <see cref="ItemSpacingProperty"/> changes
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="e"></param>
+		private static void DirectionChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
+		{
+			if (s is ItemsContainer container && e.NewValue != e.OldValue)
+			{
+				container.UpdateDirectionOnUnderylingPanel();
+			}
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Assigns the spacing to the <see cref="UnderlyingPanel"/> and forces it to update UI
+		/// </summary>
+		private void UpdateItemSpacingOnUnderylingPanel()
+		{			
+			if (UnderlyingPanel != null)
+			{
+				UnderlyingPanel.ItemSpacing = ItemSpacing;				
+			}
+		}
+
+		/// <summary>
+		/// Assigns the spacing to the <see cref="UnderlyingPanel"/> and forces it to update UI
+		/// </summary>
+		private void UpdateDirectionOnUnderylingPanel()
+		{
+			if (UnderlyingPanel != null)
+			{
+				UnderlyingPanel.FlowDirection = FlowDirection;				
+			}
+		}
+
+		#endregion
 	}
 }
--- UWPEnhanced/Controls/ItemsContainer/ItemsContainerPanel.cs
+++ UWPEnhanced/Controls/ItemsContainer/ItemsContainerPanel.cs
@@ -4,24 +4,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace UWPEnhanced.Controls
 {
 	internal class ItemsContainerPanel : Panel
 	{
-		public ItemsDirection Direction { get; set; } = ItemsDirection.ToBottom;
+		#region Private Properties
 
-		public double ItemSpacing { get; set; } = 0;
+		/// <summary>
+		/// The total space between the items in the panel
+		/// </summary>
+		/// <returns></returns>
+		private double TotalItemSpacing => Children.Count > 1 ? (Children.Count - 1) * ItemSpacing : 0;
 
+		#endregion
 
-		protected override Size ArrangeOverride(Size finalSize)
+		#region FlowDirection Dependency Property
+
+		/// <summary>
+		/// The direction to present the items in. Hides the inherited <see cref="FrameworkElement.FlowDirection"/>.
+		/// Change in value will update the UI automatically
+		/// </summary>
+		public new ItemsDirection FlowDirection
 		{
-			return ArrangeToRight(finalSize);
-
+			get => (ItemsDirection)GetValue(FlowDirectionProperty);
+			set => SetValue(FlowDirectionProperty, value);
 		}
 
-		private Size ArrangeToBottom(Size finalSize)
+		/// <summary>
+		/// Backing store for <see cref="FlowDirection"/>
+		/// </summary>
+		public new static readonly DependencyProperty FlowDirectionProperty =
+			DependencyProperty.Register(nameof(FlowDirection), typeof(ItemsDirection),
+			typeof(ItemsContainerPanel), new PropertyMetadata(ItemsContainer.DefaultFlowDirection, FlowDirectionChanged));
+
+		#endregion
+
+		#region ItemSpacing Dependency Property
+
+		/// <summary>
+		/// Spacing between two neighbouring items. Change in value will update the UI automatically.
+		/// </summary>
+		public double ItemSpacing
+		{
+			get => (double)GetValue(ItemSpacingProperty);
+			set => SetValue(ItemSpacingProperty, value);
+		}
+
+		/// <summary>
+		/// Backing store for <see cref="ItemSpacing"/>
+		/// </summary>
+		public static readonly DependencyProperty ItemSpacingProperty =
+			DependencyProperty.Register(nameof(ItemSpacing), typeof(double),
+			typeof(ItemsContainerPanel), new PropertyMetadata(ItemsContainer.DefaultItemSpacing, ItemSpacingChanged));
+
+		#endregion
+
+		#region Private static methods
+
+		/// <summary>
+		/// Callback for <see cref="ItemSpacingProperty"/> changed, updates the UI if the new value differs from the old one
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="e"></param>
+		private static void ItemSpacingChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
+		{
+			if(s is ItemsContainerPanel panel && e.NewValue != e.OldValue)
+			{
+				panel.InvalidateMeasure();
+			}
+		}
+
+		/// <summary>
+		/// Callback for <see cref="FlowDirectionProperty"/> changed, updates the UI if the new value differs from the old one
+		/// </summary>
+		/// <param name="s"></param>
+		/// <param name="e"></param>
+		private static void FlowDirectionChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
+		{
+			if (s is ItemsContainerPanel panel && e.NewValue is ItemsDirection newDirection &&
+				e.OldValue is ItemsDirection oldDirection && newDirection != oldDirection)
+			{
+				// If the change didn't change the axis of item placement (ex: old and new placement are both vertical)
+				if((newDirection == ItemsDirection.LeftToRight && oldDirection == ItemsDirection.RightToLeft) ||
+					(newDirection == ItemsDirection.RightToLeft && oldDirection == ItemsDirection.LeftToRight) ||
+					(newDirection == ItemsDirection.TopToBottom && oldDirection == ItemsDirection.BottomToTop) ||
+					(newDirection == ItemsDirection.BottomToTop && oldDirection == ItemsDirection.TopToBottom))
+				{
+					// Then only rearrange the items
+					panel.InvalidateArrange();
+				}
+				else
+				{
+					// Otherwise remeasure and rearrange
+					panel.InvalidateMeasure();
+				}
+			}
+		}
+
+		#endregion
+
+		/// <summary>
+		/// Arranges the items in a top to bottom direction. Helper of <see cref="ArrangeOverride(Size)"/>.
+		/// </summary>
+		/// <param name="finalSize"></param>
+		/// <returns></returns>
+		private Size ArrangeTopToBottom(Size finalSize)
 		{
 			double cumulativeHeight = 0;
 
@@ -35,7 +125,12 @@ namespace UWPEnhanced.Controls
 			return finalSize;
 		}
 
-		private Size ArrangeToTop(Size finalSize)
+		/// <summary>
+		/// Arranges the items in a bottom to top direction. Helper of <see cref="ArrangeOverride(Size)"/>.
+		/// </summary>
+		/// <param name="finalSize"></param>
+		/// <returns></returns>
+		private Size ArrangeBottomToTop(Size finalSize)
 		{
 			double cumulativeHeight = 0;
 
@@ -54,9 +149,12 @@ namespace UWPEnhanced.Controls
 			return finalSize;
 		}
 
-
-
-		private Size ArrangeToLeft(Size finalSize)
+		/// <summary>
+		/// Arranges the items left to right. Helper of <see cref="ArrangeOverride(Size)"/>.
+		/// </summary>
+		/// <param name="finalSize"></param>
+		/// <returns></returns>
+		private Size ArrangeLeftToRight(Size finalSize)
 		{
 			double cumulativeWidth = 0;
 
@@ -70,7 +168,12 @@ namespace UWPEnhanced.Controls
 			return finalSize;
 		}
 
-		private Size ArrangeToRight(Size finalSize)
+		/// <summary>
+		/// Arranges the items right to left. Helper of <see cref="ArrangeOverride(Size)"/>.
+		/// </summary>
+		/// <param name="finalSize"></param>
+		/// <returns></returns>
+		private Size ArrangeRightToLeft(Size finalSize)
 		{
 			double cumulativeWidth = 0;
 
@@ -89,16 +192,15 @@ namespace UWPEnhanced.Controls
 			return finalSize;
 		}
 
-		protected override Size MeasureOverride(Size availableSize)
-		{
-			return (Direction == ItemsDirection.ToLeft || Direction == ItemsDirection.ToRight) ?
-				MeasureWhenHorizontal(availableSize) : MeasureWhenVertical(availableSize);
-		}
-
+		/// <summary>
+		/// Measures the total size of the panel in horizontal position. Helper of <see cref="MeasureOverride(Size)"/>.
+		/// </summary>
+		/// <param name="availableSize"></param>
+		/// <returns></returns>
 		private Size MeasureWhenHorizontal(Size availableSize)
 		{
 			double greatestHeight = 0;
-			double cumulativeWidth = 0;
+			double cumulativeChildrenWidth = 0;
 
 			// For each child
 			foreach (var item in Children)
@@ -107,34 +209,90 @@ namespace UWPEnhanced.Controls
 				item.Measure(availableSize);
 
 				// Add its width to the total width of the panel
-				cumulativeWidth += item.DesiredSize.Width;
+				cumulativeChildrenWidth += item.DesiredSize.Width;
 
 				// If its height is the greatest save it
 				greatestHeight = Math.Max(greatestHeight, item.DesiredSize.Height);
 			}
 
-			return new Size(cumulativeWidth, greatestHeight);
+			return new Size(cumulativeChildrenWidth + TotalItemSpacing, greatestHeight);
 		}
 
+		/// <summary>
+		/// Measures the total size in vertical position. Helper of <see cref="MeasureOverride(Size)"/>.
+		/// </summary>
+		/// <param name="availableSize"></param>
+		/// <returns></returns>
 		private Size MeasureWhenVertical(Size availableSize)
 		{
 			double greatestWidth = 0;
-			double cumulativeHeight = 0;
+			double cumulativeChildrenHeight = 0;
 
 			// For each child
 			foreach (var item in Children)
 			{
 				// Measure it
 				item.Measure(availableSize);
-				
+
 				// If its width is the greatest save it
 				greatestWidth = Math.Max(greatestWidth, item.DesiredSize.Width);
 
 				// Add its height to the total height of the panel
-				cumulativeHeight += item.DesiredSize.Height;
+				cumulativeChildrenHeight += item.DesiredSize.Height;
 			}
 
-			return new Size(greatestWidth, cumulativeHeight);
+			return new Size(greatestWidth, cumulativeChildrenHeight + TotalItemSpacing);
 		}
+		
+		#region Protected Methods
+
+		/// <summary>
+		/// Rearranges the items in the desired direction
+		/// </summary>
+		/// <param name="finalSize"></param>
+		/// <returns></returns>
+		protected override Size ArrangeOverride(Size finalSize)
+		{
+			switch (FlowDirection)
+			{
+				case ItemsDirection.RightToLeft:
+					{
+						return ArrangeRightToLeft(finalSize);
+					}
+
+				case ItemsDirection.BottomToTop:
+					{
+						return ArrangeBottomToTop(finalSize);
+					}
+
+				case ItemsDirection.LeftToRight:
+					{
+						return ArrangeLeftToRight(finalSize);
+					}
+
+				case ItemsDirection.TopToBottom:
+					{
+						return ArrangeTopToBottom(finalSize);
+					}
+
+				default:
+					{
+						throw new Exception("Unsupported case; This shouldn't happen");
+					}
+			}
+		}
+
+		/// <summary>
+		/// Measures the total size of the container
+		/// </summary>
+		/// <param name="availableSize"></param>
+		/// <returns></returns>
+		protected override Size MeasureOverride(Size availableSize)
+		{
+			return (FlowDirection == ItemsDirection.RightToLeft || FlowDirection == ItemsDirection.LeftToRight) ?
+				MeasureWhenHorizontal(availableSize) : MeasureWhenVertical(availableSize);
+		}
+
+		#endregion		
 	}
 }
