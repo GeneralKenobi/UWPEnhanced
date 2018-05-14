5365eda71d55a342053e26d16eac4f2337ededa9
Szymon Żogała <szymek.655@gmail.com>
2018-05-14 22:31:44 +0200

    Added

---
+++ UWPEnhanced/Controls/Menu/Converters/MenuPositionToItemsDirectionConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace UWPEnhanced.Controls
+{
+	/// <summary>
+	/// If value is a <see cref="MenuPosition"/>, returns a proper <see cref="ItemsDirection"/> for the IconsPanel.
+	/// Otherwise returns <see cref="ItemsDirection.TopToBottom"/>
+	/// </summary>
+	public class MenuPositionToItemsDirectionConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if(value is MenuPosition position)
+			{
+				switch (position)
+				{
+					case MenuPosition.Left:
+					case MenuPosition.Right:
+						{
+							return ItemsDirection.TopToBottom;
+						}
+
+					case MenuPosition.Top:
+					case MenuPosition.Bottom:
+						{
+							return ItemsDirection.LeftToRight;
+						}
+				}
+			}
+
+			return ItemsDirection.TopToBottom;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
