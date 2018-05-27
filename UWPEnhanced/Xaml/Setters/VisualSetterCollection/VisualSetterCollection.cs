becff9289aabcd4580ff2719f942e5598eb664d8
Szymon Żogała <szymek.655@gmail.com>
2018-05-27 22:21:37 +0200

    Added

---
+++ UWPEnhanced/Xaml/Setters/VisualSetterCollection/VisualSetterCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// This class's purpose is to be able to create such collection in xaml because generic classes cannot be created in xaml
+	/// </summary>
+	public class VisualSetterCollection : DependencyObjectCollectionOfT<IVisualSetter>
+	{
+
+	}
+}
