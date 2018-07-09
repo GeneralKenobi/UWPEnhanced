e73c6129575e0978d02dc6f05450b8ee26093c90
Szymon Żogała <szymek.655@gmail.com>
2018-07-09 19:06:51 +0200

    Added

---
+++ UWPEnhanced/Xaml/VisualTriggers/VisualRoutedEventTriggers/Enums/RelativeTo.cs
@@ -0,0 +1,24 @@
+﻿namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// Enumeration for visual trigger commands with command parameter equal to position of pointer relative to something
+	/// </summary>
+	public enum RelativeTo
+	{
+		/// <summary>
+		/// Relative to the the command is attached to (if that object is a UIElement, otherwise null is passed which results
+		/// in <see cref="Window"/>
+		/// </summary>
+		AttachedTo = 0,
+
+		/// <summary>
+		/// Relative to the overall window
+		/// </summary>
+		Window = 1,
+
+		/// <summary>
+		/// Relative to a UIElement bound to the RelativeToElement dependency property
+		/// </summary>
+		BoundObject	= 2,
+	}
+}
