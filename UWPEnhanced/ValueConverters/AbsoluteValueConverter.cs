bb56804f39a9221ba58e64b89725e89928b6ffa8
Szymon Żogała <szymek.655@gmail.com>
2018-07-27 21:36:02 +0200

    Added

---
+++ UWPEnhanced/ValueConverters/AbsoluteValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace UWPEnhanced.ValueConverters
+{
+	/// <summary>
+	/// If the value is a double returns its absolute value, otherwise returns 0
+	/// </summary>
+	public class AbsoluteValueConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (value is double dValue)
+			{
+				return Math.Abs(dValue);
+			}
+
+			return 0d;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
