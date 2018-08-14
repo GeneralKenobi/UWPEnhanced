682c6d29882c7da8129543fbc1b87b7b9894db3b
Szymon Żogała <szymek.655@gmail.com>
2018-08-14 23:29:15 +0200

    Added

---
+++ UWPEnhanced/Xaml/VisualTriggers/VisualDataTrigger/VisualDataTriggerEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// Event args for <see cref="GenericVisualDataTrigger{T}"/>
+	/// </summary>
+	public class VisualDataTriggerEventArgs<T> : EventArgs
+	{
+		#region Constructor
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public VisualDataTriggerEventArgs(T value)
+		{
+			Value = value;
+		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// Value that triggered the <see cref="IVisualTrigger{T}"/>
+		/// </summary>
+		public T Value { get; }
+
+		#endregion
+	}
+}
