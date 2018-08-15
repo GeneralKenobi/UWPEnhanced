cc528e40c8f5869049d41652939ab4fd31185715
Szymon Żogała <szymek.655@gmail.com>
2018-08-15 22:49:57 +0200

    Moved a lot of logic to VisualRoutedEventTrigger, some specific behaviour may be obtained by overriding virtual/abstract protected methods. In gerenal inheriting classes have much less implementation to do.

--- UWPEnhanced/Xaml/VisualTriggers/VisualRoutedEventTriggers/VisualRoutedEventTrigger.cs
+++ UWPEnhanced/Xaml/VisualTriggers/VisualRoutedEventTriggers/VisualRoutedEventTrigger.cs
@@ -5,25 +5,17 @@ namespace UWPEnhanced.Xaml
 {
 	/// <summary>
 	/// Base class for visual triggers that operate based on some routed events and that may set the handled property of those
-	/// events to prevent them from bubbling up the visual tree. <typeparamref name="T"/> is the type of the RoutedEventArgs for
-	/// specific implementations.
+	/// events to prevent them from bubbling up the visual tree. <typeparamref name="T"/> is the specific type of the RoutedEventArgs
+	/// for specific implementations.
 	/// </summary>
-	public class VisualRoutedEventTrigger<T> : VisualAttachment, IVisualTrigger<T>
+	public abstract class VisualRoutedEventTrigger<T> : VisualTrigger
 		where T : RoutedEventArgs
 	{
-		#region Events
-
-		/// <summary>
-		///  Event fired when the criteria for trigger are met (appropriate pointer event with matching modifiers)
-		/// </summary>
-		public EventHandler<T> Triggered { get; set; }
-
-		#endregion
-
 		#region SetHandled Dependency Property
 
 		/// <summary>
-		/// Sets the handled property on the intercepted <see cref="RoutedEventArgs"/> to this value
+		/// Sets the handled property on the intercepted <see cref="RoutedEventArgs"/> to this value (if the given
+		/// <see cref="RoutedEventArgs"/> has that property (it usually does))
 		/// </summary>
 		public bool SetHandled
 		{
@@ -46,9 +38,9 @@ namespace UWPEnhanced.Xaml
 		/// Target to listen to (attach to) instead of the default one determined by means of <see cref="VisualAttachment"/>'s
 		/// inherited methods. To use the default one this property should be left unset
 		/// </summary>
-		public FrameworkElement Target
+		public UIElement Target
 		{
-			get => (FrameworkElement)GetValue(TargetProperty);
+			get => (UIElement)GetValue(TargetProperty);
 			set => SetValue(TargetProperty, value);
 		}
 
@@ -56,28 +48,117 @@ namespace UWPEnhanced.Xaml
 		/// Backing store for <see cref="Target"/>
 		/// </summary>
 		public static readonly DependencyProperty TargetProperty =
-			DependencyProperty.Register(nameof(Target), typeof(FrameworkElement),
+			DependencyProperty.Register(nameof(Target), typeof(UIElement),
 			typeof(VisualRoutedEventTrigger<T>),
-			new PropertyMetadata(default(FrameworkElement), new PropertyChangedCallback(TargetChangedCallback)));
+			new PropertyMetadata(default(UIElement), new PropertyChangedCallback(TargetChangedCallback)));
+
+		#endregion
+
+		#region Protected methods
+
+		/// <summary>
+		/// Subscribes to the specific RoutedEvent
+		/// </summary>
+		protected abstract void Subscribe();
+
+		/// <summary>
+		/// Unsubscribes from the specific RoutedEvent
+		/// </summary>
+		protected abstract void Unsubcribe();
+
+		/// <summary>
+		/// Sets the Handled property on a specific <see cref="RoutedEventArgs"/>
+		/// </summary>
+		/// <param name="e"></param>
+		protected abstract void AssignHandled(T e);
+
+		/// <summary>
+		/// Check method, event won't be fired if it returns false. By default returns true. May be overriden to provide specific checks
+		/// </summary>
+		/// <returns></returns>
+		protected virtual bool TriggerConditionsMet() => true;
+
+		/// <summary>
+		/// Method calling <see cref="AssignHandled(T)"/> and then firing the Triggered event
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		protected void TriggerEvent(object sender, T e)
+		{
+			if (TriggerConditionsMet())
+			{
+				// Let the derived classes set the Handled property on their specific RoutedEventArgs
+				AssignHandled(e);
+
+				// Invoke the event
+				Triggered?.Invoke(this, e);
+			}
+		}
+
+		#endregion
+
+		#region Public methods
+
+		/// <summary>
+		/// On top of the standard functionality, it unsubscribes from the old <see cref="UIElement"/> and subscribes to
+		/// the new one
+		/// </summary>
+		/// <param name="obj"></param>
+		public override void Attach(DependencyObject obj)
+		{
+			if (AttachedTo != null)
+			{
+				Unsubcribe();
+			}
+
+			// Only UIElements and above have routed events
+			if (obj is UIElement)
+			{
+				base.Attach(obj);
+
+				if (AttachedTo != null)
+				{
+					Subscribe();
+				}
+			}
+			else
+			{
+				throw new ArgumentException("Routed Events are introduced in " + nameof(UIElement) + "; " +
+					nameof(obj) + " has to derive from " + nameof(UIElement));
+			}
+		}
+
+		/// <summary>
+		/// On top of the standard functionality also unsubscribes from the detatched object
+		/// </summary>
+		public override void Detach()
+		{
+			if (AttachedTo != null)
+			{
+				Unsubcribe();
+			}
+
+			base.Detach();
+		}
 
 		#endregion
 
 		#region Private Static Methods
 
 		/// <summary>
-		/// Detatches the old value and attaches then new one (if it's not null)
+		/// Detatches the old value and attaches then new one (if it's not null and is an <see cref="UIElement"/>)
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="args"></param>
 		private static void TargetChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs args)
 		{
 			// If sender is an instance of VisualRoutedEventArgs<T>
-			if (sender is VisualRoutedEventTrigger<T> trigger && args.NewValue is FrameworkElement newElement)
+			if (sender is VisualRoutedEventTrigger<T> trigger && args.NewValue is UIElement newElement)
 			{
-				// Detatch from the current FrameworkElement
+				// Detatch from the current UIElement
 				trigger.Detach();
 
-				// And attach to the new FrameworkElement
+				// And attach to the new UIElement
 				trigger.Attach(newElement);
 			}
 		}
