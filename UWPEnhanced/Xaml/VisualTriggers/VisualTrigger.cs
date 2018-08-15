17fd14a57c3a0f804407fbb2d7a5da781afbf6e3
Szymon Żogała <szymek.655@gmail.com>
2018-08-15 22:45:48 +0200

    Added as a standard implementation of IVisualTrigger

---
+++ UWPEnhanced/Xaml/VisualTriggers/VisualTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// Standard implementation of <see cref="IVisualTrigger"/>
+	/// </summary>
+	public class VisualTrigger : VisualAttachment, IVisualTrigger
+	{
+		/// <summary>
+		/// Notifies when the trigger is fired
+		/// </summary>
+		public EventHandler<object> Triggered { get; set; }
+	}
+}
