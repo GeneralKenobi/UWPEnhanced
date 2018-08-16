302cfce5de2e31d13fb09a9877e8e6cfd3ef9167
Szymon Żogała <szymek.655@gmail.com>
2018-08-16 11:32:41 +0200

    Added as new version of pointer position related visual commands

---
+++ UWPEnhanced/Xaml/VisualCommands/PointerPositionRelatedVisualCommands/PointerPositionVisualCommand/PointerPositionVisualCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Numerics;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// A class for in-xaml binding of commands that are triggered by pointer routed events and whose parameter is the pointer position
+	/// relative to some entity. The parameter is given as a <see cref="Complex"/>
+	/// </summary>
+	public class PointerPositionVisualCommand : BasePointerPositionVisualCommand<VisualPointerTrigger, PointerRoutedEventArgs>
+	{
+		#region Protected methods
+
+		/// <summary>
+		/// Executes the command with the position of the cursor relative to <see cref="RelativeToElement"/>. If <see cref="RelativeToElement"/>
+		/// is null, gets the position relative to <see cref="IAttachable.AttachedTo"/> (if it's an <see cref="UIElement"/>, if not
+		/// then an exception is thrown). The position is passed as a <see cref="Complex"/>
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		protected override void TriggerFiredCallback(object sender, PointerRoutedEventArgs e)
+		{
+			PointerPoint pointerInfo = null;
+
+			// Determine the position relative to the chosen element
+			switch (RelativeToOption)
+			{
+				case RelativeTo.AttachedTo:
+					{
+						pointerInfo = e.GetCurrentPoint(AttachedTo as UIElement);
+					}
+					break;
+
+				case RelativeTo.BoundObject:
+					{
+						pointerInfo = e.GetCurrentPoint(RelativeToElement);
+					}
+					break;
+
+				case RelativeTo.Window:
+					{
+						pointerInfo = e.GetCurrentPoint(null);
+					}
+					break;
+
+				default:
+					{
+						throw new Exception("Unsupported case");
+					}
+			}
+
+			// Create a new Complex off of it
+			var param = new Complex(pointerInfo.Position.X, pointerInfo.Position.Y);
+
+			// Execute the command (and use the converter if it was provided)
+			Command?.Execute(Converter == null ? param : Converter.Convert(param));
+		}
+
+		#endregion
+	}
+}
---
+++ UWPEnhanced/Xaml/VisualCommands/PointerPositionRelatedVisualCommands/TappedPointerPositionVisualCommand/TappedPointerPositionVisualCommand.cs
@@ -0,0 +1,58 @@
+﻿using System.Numerics;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Input;
+
+namespace UWPEnhanced.Xaml
+{
+	/// <summary>
+	/// A class for in-xaml binding of commands that are triggered by tapped routed event and use the pointer position as the command's
+	/// parameter
+	/// </summary>
+	public class TappedPointerPositionVisualCommand : BasePointerPositionVisualCommand<VisualTappedTrigger, TappedRoutedEventArgs>
+	{
+		#region Protected methods
+
+		/// <summary>
+		/// Executes the command with the position of the cursor relative to <see cref="RelativeToElement"/>. If <see cref="RelativeToElement"/>
+		/// is null, gets the position relative to <see cref="IAttachable.AttachedTo"/> (if it's an <see cref="UIElement"/>, if not
+		/// then an exception is thrown). The position is passed as a <see cref="Complex"/>
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		protected override void TriggerFiredCallback(object sender, TappedRoutedEventArgs e)
+		{
+			Point position;
+
+			// Determine the position relative to the chosen element
+			switch (RelativeToOption)
+			{
+				case RelativeTo.AttachedTo:
+					{
+						position = e.GetPosition(AttachedTo as UIElement);
+					}
+					break;
+
+				case RelativeTo.BoundObject:
+					{
+						position = e.GetPosition(RelativeToElement);
+					}
+					break;
+
+				case RelativeTo.Window:
+					{
+						position = e.GetPosition(null);
+					}
+					break;
+			}
+
+			// Create a new Complex off of it
+			var param = new Complex(position.X, position.Y);
+
+			// Execute the command (and use the converter if it was provided)
+			Command?.Execute(Converter == null ? param : Converter.Convert(param));		
+		}
+
+		#endregion
+	}
+}
