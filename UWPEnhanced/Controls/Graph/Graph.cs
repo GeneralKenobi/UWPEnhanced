220d7beca81d32ddc1dc787de73f4dcdaf45947f
Szymon Żogała <szymek.655@gmail.com>
2018-08-25 21:49:35 +0200

    Added

---
+++ UWPEnhanced/Controls/Graph/Graph.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace UWPEnhanced.Controls
+{
+	/// <summary>
+	/// Control presenting X-Y data on a graph
+	/// </summary>
+	public sealed class Graph : Control
+	{
+		#region Constructors
+
+		/// <summary>
+		/// Default constructor
+		/// </summary>
+		public Graph()
+		{
+			this.DefaultStyleKey = typeof(Graph);
+		}
+
+		#endregion
+	}
+}
