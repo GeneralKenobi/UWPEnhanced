27065fdbd0c0efd171c19494bf41ba0be43afbda
Szymon Żogała <szymek.655@gmail.com>
2018-08-27 22:55:37 +0200

    Added

---
+++ UWPEnhanced/ValueConverters/MarkupExtensionBased/SumConverter/SumConverter.cs
@@ -0,0 +1,74 @@
+﻿using CSharpEnhanced.Maths;
+using System;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+
+namespace UWPEnhanced.ValueConverters
+{
+	public partial class SumMEC : MarkupExtension
+	{
+		/// <summary> 
+		/// If value is a numeral type (except <see cref="Complex"/>), it is cast to double and <see cref="_Summand"/> is added to it.
+		/// Otherwise returns the value without changing it.
+		/// </summary>
+		private class SumConverter : IValueConverter
+		{
+			#region Constructors
+
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			public SumConverter(double summand)
+			{
+				_Summand = summand;
+			}
+
+			#endregion
+
+			#region Private properties
+
+			/// <summary>
+			/// Number to add to the value
+			/// </summary>
+			private double _Summand { get; }
+
+			#endregion
+
+			#region Public methods
+
+			/// <summary>			
+			/// If value is a numeral type (except <see cref="Complex"/>), it is cast to double and <see cref="_Summand"/> is added to it.
+			/// Otherwise returns the value without changing it.
+			/// </summary>
+			/// <param name="value"></param>
+			/// <param name="targetType"></param>
+			/// <param name="parameter"></param>
+			/// <param name="language"></param>
+			/// <returns></returns>
+			public object Convert(object value, Type targetType, object parameter, string language)
+			{
+				if (MathsHelpers.TryCastToDouble(value, out var casted))
+				{
+					return casted * _Summand;
+				}
+
+				return value;
+			}
+
+			/// <summary>
+			/// Not implemented
+			/// </summary>
+			/// <param name="value"></param>
+			/// <param name="targetType"></param>
+			/// <param name="parameter"></param>
+			/// <param name="language"></param>
+			/// <returns></returns>
+			public object ConvertBack(object value, Type targetType, object parameter, string language)
+			{
+				throw new NotImplementedException();
+			}
+
+			#endregion
+		}
+	}
+}
---
+++ UWPEnhanced/ValueConverters/MarkupExtensionBased/SumConverter/SumMEC.cs
@@ -0,0 +1,32 @@
+﻿using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+
+namespace UWPEnhanced.ValueConverters
+{
+	/// <summary> 
+	/// If value is a numeral type (except <see cref="Complex"/>), it is cast to double and <see cref="Summand"/> is added to it.
+	/// Otherwise returns the value without changing it. By default <see cref="Summand"/> is 0.
+	/// </summary>
+	[MarkupExtensionReturnType(ReturnType = typeof(IValueConverter))]
+	public partial class SumMEC : MarkupExtension
+	{
+		#region Public properties
+
+		/// <summary>
+		/// Number to add to the value. By default it's 0.
+		/// </summary>
+		public double Summand { get; set; }
+
+		#endregion
+
+		#region Protected methods
+
+		/// <summary>
+		/// Returns a new <see cref="SumConverter"/>
+		/// </summary>
+		/// <returns></returns>
+		protected override object ProvideValue() => new SumConverter(Summand);
+
+		#endregion
+	}
+}
