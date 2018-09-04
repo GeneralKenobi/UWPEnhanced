9c374c2ddc4fcf7cf2c8918bf76ec6f9ac077c8f
Szymon Żogała <szymek.655@gmail.com>
2018-09-04 14:29:37 +0200

    Changed the default value for SelectedSides

--- UWPEnhanced/ValueConverters/MarkupExtensionBased/DoubleToUniformThickness/DoubleToUniformThicknessMEC.cs
+++ UWPEnhanced/ValueConverters/MarkupExtensionBased/DoubleToUniformThickness/DoubleToUniformThicknessMEC.cs
@@ -7,7 +7,9 @@ namespace UWPEnhanced.ValueConverters
 	/// If value is a numeral type, it is cast to double and a new thickness with all entries marked in
 	/// <see cref="SelectedSides"/> equal to the value times <see cref="Multiplier"/> (and all entries not marked in
 	/// <see cref="SelectedSides"/> equal to 0) is returned. If value is not a numeral type, returns thickness with all
-	/// entries equal to 0. By default <see cref="Multiplier"/> is equal to 1.
+	/// entries equal to 0.
+	/// By default <see cref="Multiplier"/> is equal to 1.
+	/// By default <see cref="SelectedSides"/> is equal to <see cref="Side.All"/>.
 	/// </summary>
 	[MarkupExtensionReturnType(ReturnType = typeof(IValueConverter))]
 	public partial class DoubleToUniformThicknessMEC : MarkupExtension
@@ -15,9 +17,9 @@ namespace UWPEnhanced.ValueConverters
 		#region Public properties
 
 		/// <summary>
-		/// Sides that whose values are returned (all others are set to 0)
+		/// Sides that whose values are returned (all others are set to 0). By default it's <see cref="Side.All"/>
 		/// </summary>
-		public Side SelectedSides { get; set; }
+		public Side SelectedSides { get; set; } = Side.All;
 
 		/// <summary>
 		/// Mutliples the final thickness by this value. By default it's 1.
