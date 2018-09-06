076bd665a6847a87150ef38e75f1dcbaca0565d1
Szymon Żogała <szymek.655@gmail.com>
2018-09-06 21:39:11 +0200

    Now derive from BaseFlowDirectionContainer and BaseFlowDirectionContainerPanel, removed functionality that is present in the new base class

--- UWPEnhanced/Controls/ItemsSourceBasedContainers/EqualChildSpaceContainer/CodeBehind/EqualChildSpaceContainer.cs
+++ UWPEnhanced/Controls/ItemsSourceBasedContainers/EqualChildSpaceContainer/CodeBehind/EqualChildSpaceContainer.cs
@@ -1,14 +1,11 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-
-namespace UWPEnhanced.Controls
+﻿namespace UWPEnhanced.Controls
 {
 	/// <summary>
 	/// Each child is given equal length in the expanding dimension. If the container's length in the expanding dimension is infinite,
 	/// each child is given its desired width in that direction. Non-expanding length given to each child is the maximum out of lengths
 	/// desired by any of the children and the available length in that dimension.
 	/// </summary>
-	public sealed class EqualChildSpaceContainer : ItemsControl
+	public sealed class EqualChildSpaceContainer : BaseFlowDirectionContainer
     {
 		#region Constructor
 
@@ -18,94 +15,7 @@ namespace UWPEnhanced.Controls
 		public EqualChildSpaceContainer()
         {
             this.DefaultStyleKey = typeof(EqualChildSpaceContainer);
-			this.Loaded += ItemsContainerLoaded;
         }
-
-		#endregion
-
-		#region Private members
-
-		/// <summary>
-		/// Backing store for <see cref="UnderlyingPanel"/>
-		/// </summary>
-		private EqualChildSpaceContainerPanel mUnderlyingPanel = null;
-
-		#endregion
-
-		#region Public Properties
-
-		/// <summary>
-		/// The default direction of items
-		/// </summary>
-		public static ItemsDirection DefaultFlowDirection { get; } = ItemsDirection.TopToBottom;
-
-		#endregion
-
-		#region FlowDirection Dependency Property
-
-		/// <summary>
-		/// Direction of item placement in this container. Hides the inherited <see cref="FrameworkElement.FlowDirection"/>.
-		/// </summary>
-		public new ItemsDirection FlowDirection
-		{
-			get => (ItemsDirection)GetValue(FlowDirectionProperty);
-			set => SetValue(FlowDirectionProperty, value);
-		}
-
-		/// <summary>
-		/// Backing store for <see cref="FlowDirection"/>
-		/// </summary>
-		public new static readonly DependencyProperty FlowDirectionProperty =
-			DependencyProperty.Register(nameof(FlowDirection), typeof(ItemsDirection),
-			typeof(EqualChildSpaceContainer), new PropertyMetadata(DefaultFlowDirection, DirectionChanged));
-
-		#endregion
-
-		#region Private Methods
-
-		/// <summary>
-		/// Checks whether <see cref="ItemsControl.ItemsPanelRoot"/> is a <see cref="EqualChildSpaceContainerPanel"/>, if so assigns it
-		/// to <see cref="mUnderlyingPanel"/> and assigns initial dependency property values to it
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		private void ItemsContainerLoaded(object sender, RoutedEventArgs e)
-		{
-			if (ItemsPanelRoot is EqualChildSpaceContainerPanel container)
-			{
-				mUnderlyingPanel = container;
-
-				UpdateDirectionOnUnderylingPanel();
-			}
-		}
-
-		/// <summary>
-		/// Assigns the <see cref="FlowDirection"/> to the <see cref="UnderlyingPanel"/> and forces it to update UI
-		/// </summary>
-		private void UpdateDirectionOnUnderylingPanel()
-		{
-			if (mUnderlyingPanel != null)
-			{
-				mUnderlyingPanel.FlowDirection = FlowDirection;				
-			}
-		}
-
-		#endregion
-
-		#region Private static methods
-
-		/// <summary>
-		/// Callback for when <see cref="FlowDirectionProperty"/> changes
-		/// </summary>
-		/// <param name="s"></param>
-		/// <param name="e"></param>
-		private static void DirectionChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
-		{
-			if (s is EqualChildSpaceContainer container && e.NewValue != e.OldValue)
-			{
-				container.UpdateDirectionOnUnderylingPanel();
-			}
-		}
 
 		#endregion
 	}
--- UWPEnhanced/Controls/ItemsSourceBasedContainers/EqualChildSpaceContainer/EqualChildSpaceContainerPanel/EqualChildSpaceContainerPanel.cs
+++ UWPEnhanced/Controls/ItemsSourceBasedContainers/EqualChildSpaceContainer/EqualChildSpaceContainerPanel/EqualChildSpaceContainerPanel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Windows.Foundation;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 
 namespace UWPEnhanced.Controls
 {
@@ -11,31 +10,10 @@ namespace UWPEnhanced.Controls
 	/// Panel for <see cref="EqualChildSpaceContainer"/>, positions children in 4 possible configurations and gives each child an
 	/// equal amount of space in the flow direction's axis
 	/// </summary>
-	internal class EqualChildSpaceContainerPanel : Panel
+	internal class EqualChildSpaceContainerPanel : BaseFlowDirectionContainerPanel
 	{
-		#region FlowDirection Dependency Property
-
-		/// <summary>
-		/// The direction to present the items in. Hides the inherited <see cref="FrameworkElement.FlowDirection"/>.
-		/// Change in value will update the UI automatically
-		/// </summary>
-		public new ItemsDirection FlowDirection
-		{
-			get => (ItemsDirection)GetValue(FlowDirectionProperty);
-			set => SetValue(FlowDirectionProperty, value);
-		}
-
-		/// <summary>
-		/// Backing store for <see cref="FlowDirection"/>
-		/// </summary>
-		public new static readonly DependencyProperty FlowDirectionProperty =
-			DependencyProperty.Register(nameof(FlowDirection), typeof(ItemsDirection),
-			typeof(EqualChildSpaceContainerPanel), new PropertyMetadata(EqualChildSpaceContainer.DefaultFlowDirection, FlowDirectionChanged));
-
-		#endregion
-
 		#region Private Methods
-		
+
 		/// <summary>
 		/// Arranges the items in a vertical position, from top to bottom. Helper of <see cref="ArrangeOverride(Size)"/>.
 		/// </summary>
@@ -116,17 +94,6 @@ namespace UWPEnhanced.Controls
 			}
 		}
 
-		/// <summary>
-		/// Arranges all invisible children (with new rectangle located at 0,0 with width and height equal to 0)
-		/// </summary>
-		private void ArrangeInvisibleChildren()
-		{
-			foreach(var child in Children.Where((child) => child.Visibility == Visibility.Collapsed))
-			{
-				child.Arrange(new Rect(0, 0, 0, 0));
-			}
-		}
-
 		#endregion
 
 		#region Protected Methods
@@ -198,37 +165,6 @@ namespace UWPEnhanced.Controls
 		/// <param name="availableSize"></param>
 		/// <returns></returns>
 		protected override Size MeasureOverride(Size availableSize) => availableSize;
-
-		#endregion
-
-		#region Private static methods
-
-		/// <summary>
-		/// Callback for <see cref="FlowDirectionProperty"/> changed, updates the UI if the new value differs from the old one
-		/// </summary>
-		/// <param name="s"></param>
-		/// <param name="e"></param>
-		private static void FlowDirectionChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
-		{
-			if (s is EqualChildSpaceContainerPanel panel && e.NewValue is ItemsDirection newDirection &&
-				e.OldValue is ItemsDirection oldDirection && newDirection != oldDirection)
-			{
-				// If the change didn't change the axis of item placement (ex: old and new placement are both vertical)
-				if((newDirection == ItemsDirection.LeftToRight && oldDirection == ItemsDirection.RightToLeft) ||
-					(newDirection == ItemsDirection.RightToLeft && oldDirection == ItemsDirection.LeftToRight) ||
-					(newDirection == ItemsDirection.TopToBottom && oldDirection == ItemsDirection.BottomToTop) ||
-					(newDirection == ItemsDirection.BottomToTop && oldDirection == ItemsDirection.TopToBottom))
-				{
-					// Then only rearrange the items
-					panel.InvalidateArrange();
-				}
-				else
-				{
-					// Otherwise remeasure and rearrange
-					panel.InvalidateMeasure();
-				}
-			}
-		}
 
 		#endregion
 	}
