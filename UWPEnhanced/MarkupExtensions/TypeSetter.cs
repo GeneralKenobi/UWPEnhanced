d02aec54822984d3abeeb5ec52344e8023ca8614
Szymon Żogała <szymek.655@gmail.com>
2018-09-29 14:20:06 +0200

    Added

---
+++ UWPEnhanced/MarkupExtensions/TypeSetter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.UI.Xaml.Markup;
+
+namespace UWPEnhanced.MarkupExtensions
+{
+	/// <summary>
+	/// Helps with assigning <see cref="Type"/> properties in xaml
+	/// </summary>
+	[MarkupExtensionReturnType(ReturnType = typeof(Type))]
+	public class TypeSetter : MarkupExtension
+	{
+		#region Public properties
+
+		/// <summary>
+		/// Type to return as this <see cref="MarkupExtension"/>'s return value.
+		/// </summary>
+		public Type Type { get; set; }
+
+		#endregion
+
+		#region Protected methods
+
+		/// <summary>
+		/// Returns <see cref="Type"/>
+		/// </summary>
+		/// <returns></returns>
+		protected override object ProvideValue() => Type;
+
+		#endregion
+	}
+}
