14d74c0930763222e8d5f2a5e81aa34a55366868
Szymon Żogała <szymek.655@gmail.com>
2019-03-02 22:08:36 +0100

    Added DropDownControl (no code yet)

---
+++ UWPEnhanced/Controls/DropDownControl/DropDownControl.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace UWPEnhanced.Controls
+{
+	public sealed class DropDownControl : Control
+	{
+		public DropDownControl()
+		{
+			this.DefaultStyleKey = typeof(DropDownControl);
+		}
+	}
+}
